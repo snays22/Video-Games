830bdaa04b09ed61ac6f75fa52c2ebb5c3f56f53
Charles T <chal_th82@protonmail.com>
2022-07-26 18:15:07 +0200

    Ajout du fichier entity.h

---
+++ Snake/entity.h
@@ -0,0 +1,21 @@
+/*
+ ============================================
+ Name        : entity.h
+ Author      : Charles T.
+ Version     : 1.0
+ Copyright   : by default
+ Description : snake game in C using ncurses
+ ============================================
+*/
+
+#ifndef _ENTITY_H_
+#define _ENTITY_H_
+
+#include <stdint.h>
+
+typedef struct {
+  int x;
+  int y;
+} Entity;
+
+#endif
